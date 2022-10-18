4d21cb80f1f64fb4212d27cd0dc80746b0b7e950
NicoletoAerials <67210427+NicoletoAerials@users.noreply.github.com>
2022-10-18 11:19:15 -0500

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -62,7 +62,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(NCP, Ender-3)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(NCP, Nicoleto Aerials)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -137,7 +137,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender 3"
+#define CUSTOM_MACHINE_NAME "Nicoleto Aerials"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
